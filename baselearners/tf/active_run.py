95a02dfa03bbb55b413548e0a85bf836500a3614
Grant Van Horn <gvanhorn@visipedia.org>
2016-06-03 09:07:27 -0700

    more output

--- baselearners/tf/active_run.py
+++ baselearners/tf/active_run.py
@@ -57,35 +57,38 @@ results.append((0, query_counts, active_accuracy, active_f1, active_precision, a
 
 print print_str % (0, active_accuracy, test_accuracy)    
 print >> lf, print_str % (0, active_accuracy, test_accuracy)
+lf.flush()
 
-
-for i in range(1, 2*active_labels.shape[0]):
+for i in range(7, 2*active_labels.shape[0]):
   
   print "###"
   print "Starting iteration %d" % i
   
-  predictions = base_learner.batch_predict(active_features, verbose=False)
+  idx = random.sample(range(active_features.shape[0]), 100)
+  
+  predictions = base_learner.batch_predict(active_features[idx], verbose=False)
   query = np.argmin(np.abs(predictions[:,0] - predictions[:,1]))
-  query_counts[query] += 1
-
-  base_learner.update(active_features[query], active_labels[query], verbose=True)
+  #query_counts[query] += 1
+  print "Querying a %d example" % active_labels[idx][query]
+  
+  base_learner.update(active_features[idx][query], active_labels[idx][query], lr=0.01, verbose=True)
   
   if i % test_increment == 0: 
     print "---"
     print "TESTING"
     
     # subsample the features to get a speedup
-    idx = random.sample(range(active_features.shape[0]), 30)
+    #idx = random.sample(range(active_features.shape[0]), 30)
     
-    active_prediction = np.argmax(base_learner.batch_predict(active_features[idx], verbose=False), axis=1)
-    active_accuracy = metrics.accuracy_score(active_labels[idx], active_prediction)
-    active_precision, active_recall, active_f1, support = (tuple(x) for x in metrics.precision_recall_fscore_support(active_labels[idx], active_prediction))
+    active_prediction = np.argmax(base_learner.batch_predict(active_features, verbose=False), axis=1)
+    active_accuracy = metrics.accuracy_score(active_labels, active_prediction)
+    active_precision, active_recall, active_f1, support = (tuple(x) for x in metrics.precision_recall_fscore_support(active_labels, active_prediction))
     
-    idx = random.sample(range(test_features.shape[0]), 30)
+    #idx = random.sample(range(test_features.shape[0]), 30)
     
-    test_prediction = np.argmax(base_learner.batch_predict(test_features[idx], verbose=False), axis=1)
-    test_accuracy = metrics.accuracy_score(test_labels[idx], test_prediction)
-    test_precision, test_recall, test_f1, support = (tuple(x) for x in metrics.precision_recall_fscore_support(test_labels[idx], test_prediction))
+    test_prediction = np.argmax(base_learner.batch_predict(test_features, verbose=False), axis=1)
+    test_accuracy = metrics.accuracy_score(test_labels, test_prediction)
+    test_precision, test_recall, test_f1, support = (tuple(x) for x in metrics.precision_recall_fscore_support(test_labels, test_prediction))
   
     results.append((i, query_counts, active_accuracy, active_f1, active_precision, active_recall, test_accuracy, test_f1, test_precision, test_recall))
     
